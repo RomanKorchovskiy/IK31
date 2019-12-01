211b785c08e4171230dca3fdb3308be9be050065
RomanKorchovskiy <korchovskiy@gmail.com>
2019-12-01 22:17:00 +0200

    Lab_3: зроблено

--- Korchovskiy/lab3/main/views.py
+++ Korchovskiy/lab3/main/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+import os
+from datetime import datetime
 
-# Create your views here.
+
+def main(request):
+    return render(request, 'main.html', {'parameter': "test"})
+
+
+def health(request):
+    response = {'date': datetime.now().strftime("%Y-%m-%d %H:%M:%S"), 'current_page': request.path, 'server_info': os.uname(), 'client_info': request.META['HTTP_USER_AGENT']}
+    return JsonResponse(response)
+
